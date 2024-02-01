6cf1de73d31e713521bed590029df54437584fd7
Maxime Houé <maxhoue@gmail.com>
2024-02-01 14:31:00 +0100

    feat(docker): docker support is an option

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -13,3 +13,26 @@ for path in REMOVE_MONGO_PATHS:
             os.rmdir(path)
         else:
             os.unlink(path)
+
+
+docker_files = [
+    "docker-compose.yml",
+    "docker-compose.tests.yml",
+    "Dockerfile",
+    ".run/docker-compose.run.xml",
+    ".run/docker-compose tests.run.xml",
+    ".dockerignore",
+]
+
+REMOVE_DOCKER_PATHS = [
+    '{% if cookiecutter.add_docker_support != "y" %}' + file + '{% endif %}'
+    for file in docker_files
+]
+
+for path in REMOVE_DOCKER_PATHS:
+    path = path.strip()
+    if path and os.path.exists(path):
+        if os.path.isdir(path):
+            os.rmdir(path)
+        else:
+            os.unlink(path)
